5ec212038c78a88cd5f997c8e12c6e816c70d3ac
samyla921 <samylalves921@gmail.com>
2019-06-26 10:31:37 -0300

    Aula

--- app/src/main/java/com/example/danilo/appdebts/TelaInicial.java
+++ app/src/main/java/com/example/danilo/appdebts/TelaInicial.java
@@ -1,13 +1,38 @@
 package com.example.danilo.appdebts;
 
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.danilo.appdebts.database.Databasedets;
+import com.example.danilo.appdebts.database.ScriptDLL;
+
 public class TelaInicial extends AppCompatActivity {
+
+    private SQLiteDatabase mConection;
+    private Databasedets mDataHelper;
+    private ConstraintLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+
+        mLayout = findViewById(R.id.layout);
+
+        createConnection();
+    }
+
+
+    private void createConnection() {
+        try {
+            mDataHelper = new Databasedets(this);
+            mConection = mDataHelper.getWritableDatabase();
+            Snackbar.make(mLayout, R.string.sucess_conection, Snackbar.LENGTH_LONG).show();
+        } catch (SQLException e) {
+            Snackbar.make(mLayout, e.toString(), Snackbar.LENGTH_LONG).show();
+        }
     }
 }
